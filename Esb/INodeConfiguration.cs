9a9375734ae561a2b719ac8a1a111a6ec7dc7849
Simon Dallmair <xynratron@gmail.com>
2017-03-05 14:19:37 +0100

    Added comments

--- Esb/INodeConfiguration.cs
+++ Esb/INodeConfiguration.cs
@@ -1,7 +1,17 @@
-﻿namespace Esb
+﻿using System;
+using System.Net;
+
+namespace Esb
 {
     public interface INodeConfiguration
     {
+        /// <summary>
+        /// If a node is part of the current WorkServer. 
+        /// </summary>
         bool IsLocal { get; set; }
+        /// <summary>
+        /// The Address of the Node
+        /// </summary>
+        Uri Address { get; set; }
     }
 }
