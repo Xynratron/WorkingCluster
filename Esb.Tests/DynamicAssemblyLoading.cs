679005acbe87cc86ca53765c83d5e45c468868fd
Simon Dallmair <xynratron@gmail.com>
2017-05-20 12:39:18 +0200

    Dynamically Code Compiling

---
+++ Esb.Tests/DynamicAssemblyLoading.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Esb.Tests.Helper;
+using NUnit.Framework;
+using System.Reflection;
+
+namespace Esb.Tests
+{
+    [TestFixture()]
+    public class DynamicAssemblyLoading
+    {
+        [Test()]
+        public void Compile()
+        {
+            string sourceCode = "using System;\r\n" +
+                                "public class MyLib{\r\n" +
+                                "   public static void Execute()" +
+                                "   {" +
+                                "       System.Diagnostics.Debug.WriteLine(\"In Dynamic Assembly\");" +
+                                "   }\r\n" +
+                                "}";
+
+            var result = CodeCompiling.Compile(sourceCode);
+            var loadedAssemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+           
+        }
+
+    }
+}
